9fdc03a678d28656bffb8efe1e1dd407765ede92
gabisegger <gabi.segger@gmail.com>
2023-10-04 07:33:01 -0300

    criação de condição para iniciar a contagem + inserir valores no html

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,10 +1,17 @@
 function contar(){
-    const inicio = document.getElementById('txtinicio');
-    const fim = document.getElementById('txtfim');
-    const passo = document.getElementById('txtpasso');
+    const inicio = Number(document.getElementById('txtinicio').value);
+    const fim = Number(document.getElementById('txtfim').value);
+    const passo = Number(document.getElementById('txtpasso').value);
     const resultado = document.getElementById('resultado');
 
-    for(let i = inicio; i <= fim; i + passo){
-        resultado.innerText = i
+    let numbers = [];
+    if(inicio.lenght > 0 && fim.lenght > 0 && passo.lenght > 0) {
+        for(i = inicio; i <= fim; i += passo){
+            numbers.push(i);
+            console.log(numbers);
+            resultado.innerHTML = numbers.join(' ');
+        }
+    } else{
+        alert('Preencha todos os campos para iniciar a contagem!');
     }
 }
