0c30c04231c7932ea197f4439fcdda8b2a2a5bec
gabisegger <gabi.segger@gmail.com>
2023-10-06 08:55:02 -0300

    finalização do execício com verificações e contador crescente e decrescente

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,17 +1,42 @@
 function contar(){
     const inicio = Number(document.getElementById('txtinicio').value);
     const fim = Number(document.getElementById('txtfim').value);
-    const passo = Number(document.getElementById('txtpasso').value);
+    let passo = Number(document.getElementById('txtpasso').value);
     const resultado = document.getElementById('resultado');
 
     let numbers = [];
-    if(inicio.lenght > 0 && fim.lenght > 0 && passo.lenght > 0) {
-        for(i = inicio; i <= fim; i += passo){
-            numbers.push(i);
-            console.log(numbers);
-            resultado.innerHTML = numbers.join(' ');
+    resultado.innerHTML = `Contando: <br>`;
+
+    if(inicio >= 0 && fim >= 0 && passo >= 0) {
+
+        if(passo <= 0){
+
+            alert('Passo inválido! Considerando PASSO: 1');
+            passo = 1;
+
+        } 
+
+        if(inicio < fim){
+            //Contagem crescente
+            for(i = inicio; i <= fim; i += passo){
+                numbers.push(i);
+                resultado.innerHTML += ` ${i} \u{1F449}`;
+            }
+
+        } else {
+            //Contagem descrescente
+            for(i = inicio; i >= fim; i -= passo){
+                numbers.push(i);
+                resultado.innerHTML += ` ${i} \u{1F449}`;
+            }
+
         }
+
+        resultado.innerHTML += `\u{1F3C1}`;
+    
     } else{
-        alert('Preencha todos os campos para iniciar a contagem!');
+
+        resultado.innerText = "Impossível contar!";
+        
     }
 }
